cb7c1cd4308d556d70c9c90482c8093b469c8e48
Stephanie <stepht750@gmail.com>
2019-03-07 21:58:00 -0800

    addeda map to map page

--- app/src/main/java/com/example/whatsmyallergy/MapPage.java
+++ app/src/main/java/com/example/whatsmyallergy/MapPage.java
@@ -8,7 +8,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MapPage extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapPage extends AppCompatActivity implements OnMapReadyCallback {
 
     private TextView mTextMessage;
 
@@ -46,6 +53,10 @@ public class MapPage extends AppCompatActivity {
         setContentView(R.layout.activity_map_page);
         setTitle("Map");
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -53,4 +64,13 @@ public class MapPage extends AppCompatActivity {
         menuItem.setChecked(true);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
