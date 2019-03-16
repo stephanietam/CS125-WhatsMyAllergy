9a4a4b8e97a121271dd6c2a028a56e2ffa937c42
Nina <niqu.arius@gmail.com>
2019-03-15 19:38:31 -0700

    Added Pie Chart
    -Accessed through button on profile page

--- app/src/main/java/com/example/whatsmyallergy/ProfilePage.java
+++ app/src/main/java/com/example/whatsmyallergy/ProfilePage.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -53,6 +54,12 @@ public class ProfilePage extends AppCompatActivity {
         }
     };
 
+    public void onChart(View view) {
+
+        Intent intent = new Intent(ProfilePage.this, pieChart.class);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,46 +87,46 @@ public class ProfilePage extends AppCompatActivity {
         familyHistory.setAdapter(adapter);
 
 
-        //PIE CHART
-
-        PieChart pieChart = findViewById(R.id.piechart);
-        ArrayList NoOfSymptoms = new ArrayList();
-
-        NoOfSymptoms.add(new Entry(12, 0));
-        NoOfSymptoms.add(new Entry(4f, 1));
-        NoOfSymptoms.add(new Entry(7f, 2));
-        NoOfSymptoms.add(new Entry(9f, 3));
-        NoOfSymptoms.add(new Entry(1f, 4));
-        NoOfSymptoms.add(new Entry(12, 5));
-        NoOfSymptoms.add(new Entry(4f, 6));
-        NoOfSymptoms.add(new Entry(22f, 7));
-        NoOfSymptoms.add(new Entry(9f, 8));
-        NoOfSymptoms.add(new Entry(11f, 9));
-        NoOfSymptoms.add(new Entry(1f, 10));
-        PieDataSet dataSet = new PieDataSet(NoOfSymptoms, "Symptoms Per MONTH NAME");
-
-        ArrayList symptoms = new ArrayList();
-
-        symptoms.add("Runny Nose");
-        symptoms.add("Watery Eyes");
-        symptoms.add("Sneezing");
-        symptoms.add("Coughing");
-
-        symptoms.add("Itchy Eyes and Nose");
-        symptoms.add("Dark Circles Under Eyes");
-        symptoms.add("Inflamed Nasal Passages");
-        symptoms.add("Itchy Throat and Mouth");
-        symptoms.add("Skin Reactions");
-        symptoms.add("Ear Pressure");
-        symptoms.add("Fatigue");
-
-
-        PieData data = new PieData(symptoms, dataSet);
-        pieChart.setData(data);
-        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        pieChart.animateXY(5000, 5000);
-
-        /////
+//        //PIE CHART
+//
+//        PieChart pieChart = findViewById(R.id.piechart);
+//        ArrayList NoOfSymptoms = new ArrayList();
+//
+//        NoOfSymptoms.add(new Entry(12, 0));
+//        NoOfSymptoms.add(new Entry(4f, 1));
+//        NoOfSymptoms.add(new Entry(7f, 2));
+//        NoOfSymptoms.add(new Entry(9f, 3));
+//        NoOfSymptoms.add(new Entry(1f, 4));
+//        NoOfSymptoms.add(new Entry(12, 5));
+//        NoOfSymptoms.add(new Entry(4f, 6));
+//        NoOfSymptoms.add(new Entry(22f, 7));
+//        NoOfSymptoms.add(new Entry(9f, 8));
+//        NoOfSymptoms.add(new Entry(11f, 9));
+//        NoOfSymptoms.add(new Entry(1f, 10));
+//        PieDataSet dataSet = new PieDataSet(NoOfSymptoms, "Symptoms Per MONTH NAME");
+//
+//        ArrayList symptoms = new ArrayList();
+//
+//        symptoms.add("Runny Nose");
+//        symptoms.add("Watery Eyes");
+//        symptoms.add("Sneezing");
+//        symptoms.add("Coughing");
+//
+//        symptoms.add("Itchy Eyes and Nose");
+//        symptoms.add("Dark Circles Under Eyes");
+//        symptoms.add("Inflamed Nasal Passages");
+//        symptoms.add("Itchy Throat and Mouth");
+//        symptoms.add("Skin Reactions");
+//        symptoms.add("Ear Pressure");
+//        symptoms.add("Fatigue");
+//
+//
+//        PieData data = new PieData(symptoms, dataSet);
+//        pieChart.setData(data);
+//        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+//        pieChart.animateXY(5000, 5000);
+//
+//        /////
     }
 
 
